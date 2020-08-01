72d5ae1c08633839b251309dd478b98c7a049d50
SugaR256 <kuba.krapiec@outlook.com>
2020-08-02 00:26:03 +0200

    use shorthand initialization

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ use fmp_api_manager::fmp;
 #[tokio::main]
 async fn main() {
     let config = cfg::ConfigFileManager::construct_config();
-    let mut fmp_api_manager = fmp::FmpApiManager{stocks: HashMap::new(), config: config};
+    let mut fmp_api_manager = fmp::FmpApiManager{stocks: HashMap::new(), config};
     fmp_api_manager.update_prices().await;
     for stock in fmp_api_manager.stocks.iter() {
         println!("{symbol}: {price} USD", symbol = stock.0, price = stock.1.price);
